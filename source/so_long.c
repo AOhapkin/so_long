bbf51288c690a186a367af24dfd10a16761cae07
AOhapkin <alexeyyago@gmail.com>
2022-03-30 15:50:12 +0300

    add argc check

--- source/so_long.c
+++ source/so_long.c
@@ -1,10 +1,17 @@
 #include "so_long.h"
 
+void	ft_show_error(char *error_mesaage)
+{
+	ft_putendl_fd("Error", 1);
+	ft_putendl_fd(error_mesaage, 1);
+}
+
 int	main(int argc, char **argv)
 {
-	if (argc == 2)
+	if (argc != 2)
 	{
-
+		ft_show_error("Bad arguments");
+		exit(EXIT_FAILURE);
 	}
-	
+
 }
