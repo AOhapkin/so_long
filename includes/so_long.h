470c678abc271a0684a1155418e7457cfd25cf42
AOhapkin <alexeyyago@gmail.com>
2022-04-01 14:57:52 +0300

    add get_next_line()

--- includes/so_long.h
+++ includes/so_long.h
@@ -7,6 +7,10 @@
 # include "mlx.h"
 # include "libft.h"
 
+# define GNL_BUFFER_SIZE	42
+# define GNL_DIVIDER	'\n'
+# define GNL_MAX_FD		256
+
 # define VALID_MAP_SYMBOLS	"10ECP"
 # define WALL			'1'
 # define SPACE			'0'
@@ -30,6 +34,14 @@ typedef struct s_game
 {
 	t_map	map;
 }		t_game;
+
+char	*get_next_line(int fd);
+void	free_tmp(char *tmp_for_freeing);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_substr(char const *str, unsigned int start, size_t len);
+char	*ft_strdup(const char *str);
 
 void	init_map(char *file_path, t_game *game);
 void	validate_map(t_game *game);
--- source/so_long.c
+++ source/so_long.c
@@ -1,16 +1,44 @@
 #include "so_long.h"
 
-void	ft_game_error(char *error_mesaage, t_game game)
+int	update_frames(t_game *game)
 {
-	ft_putendl_fd("Error", 1);
-	ft_putendl_fd(error_mesaage, 1);
+	static int	mainframe = -1;
+	static int	animeframe = -1;
+
+	if (animeframe >= ANIMEFRAME_INTERVAL)
+	{
+		if (game->animeframe == 1)
+			game->animeframe = -1;
+		game->animeframe++;
+		animeframe = 0;
+	}
+	animeframe++;
+	if (animeframe < 0)
+		draw_tilemap(game);
+	else if (mainframe >= MAINFRAME_INTERVAL)
+	{
+		draw_tilemap(game);
+		mainframe = 0;
+	}
+	mainframe++;
+	return (0);
 }
 
 void	start_game(char **argv)
 {
 	t_game	game;
 
-	init_map(argv[1], &game);
+	map_init(av[1], &game);
+	game.width = IMG_SIZE * game.map.cols;
+	game.height = IMG_SIZE * game.map.rows;
+	game.title = av[0];
+	game.animeframe = 0;
+	game.mlx = mlx_init();
+	game.window = mlx_new_window(game.mlx, game.width,
+								 game.height + 16, game.title);
+	hook_init(&game);
+	mlx_loop_hook(game.mlx, *update_frames, &game);
+	mlx_loop(game.mlx);
 }
 
 int	main(int argc, char **argv)
