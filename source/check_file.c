2164f5658e0d2251b87fa57383169c4408048248
AOhapkin <alexeyyago@gmail.com>
2022-04-07 09:51:26 +0300

    отрисовка карты в окне

--- source/check_file.c
+++
@@ -1,9 +0,0 @@
-#include "so_long.h"
-
-int	isfiletype(char *path, char *type)
-{
-	int	len;
-
-	len = ft_strlen(path);
-	return (ft_strncmp(path + len - ft_strlen(type), type, len) == 0);
-}
---
+++ source/image.c
@@ -0,0 +1,64 @@
+#include "so_long.h"
+
+void	put_xpm_image(int x, int y, const char *file, t_game *game)
+{
+	int	w;
+	int	h;
+
+	game->img = mlx_xpm_file_to_image(game->mlx, (char *)file, &w, &h);
+	mlx_put_image_to_window(game->mlx, game->win, game->img, x, y);
+}
+
+void	put_ground(int x, int y, char c, t_game *game)
+{
+	if (c == '1')
+	{
+		put_xpm_image(x, y, FLOOR, game);
+		put_xpm_image(x, y, WALL, game);
+	}
+	else if (c == '0')
+	{
+		put_xpm_image(x, y, FLOOR, game);
+	}
+}
+
+void	put_image(int x, int y, char c, t_game *game)
+{
+	put_ground(x, y, c, game);
+	if (c == 'C')
+	{
+		put_xpm_image(x, y, FLOOR, game);
+		put_xpm_image(x, y, COIN, game);
+	}
+	else if (c == 'P')
+	{
+		game->x = x / SCALE;
+		game->y = y / SCALE;
+		put_xpm_image(x, y, FLOOR, game);
+		put_xpm_image(x, y, PLAYER1, game);
+		game->map[game->y][game->x] = '0';
+	}
+	else if (c == 'E')
+	{
+		put_xpm_image(x, y, FLOOR, game);
+		put_xpm_image(x, y, EXIT, game);
+	}
+}
+
+void	draw_map(t_game *game)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < game->height)
+	{
+		j = 0;
+		while (j < game->width)
+		{
+			put_image(j * SCALE, i * SCALE, game->map[i][j], game);
+			++j;
+		}
+		++i;
+	}
+}
